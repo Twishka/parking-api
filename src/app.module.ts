e1dca9c1261e639265734f2084f27456d3eac165
Iryna Sadovnykova <sadovnikova.irina@gmail.com>
2018-09-17 23:29:33 +0300

    db_url

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,15 +13,22 @@ import { Car } from './entities/car.entity';
 import { User } from './entities/user.entity';
 import { Rate } from './entities/rate.entity';
 
+console.log('db-url:');
+console.log(process.env.DATABASE_URL);
+
+const db = process.env.DATABASE_URL
+  ? {url: process.env.DATABASE_URL}
+  : { host: 'lermex.site',
+    port: 18874,
+    username: 'postgres',
+    password: '212a418416367de527745b4731f94336',
+    database: 'parking_api_database'};
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
+      ...db,
       type: 'postgres',
-      host: 'lermex.site',
-      port: 18874,
-      username: 'postgres',
-      password: '212a418416367de527745b4731f94336',
-      database: 'parking_api_database',
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
