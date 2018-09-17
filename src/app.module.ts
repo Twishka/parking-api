f5ec325bb623ee8c3090c8e1659d101f5e638ade
Iryna Sadovnykova <sadovnikova.irina@gmail.com>
2018-09-17 23:14:08 +0300

    Fixed the import error

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,16 @@ import { Rate } from './entities/rate.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'lermex.site',
+      port: 18874,
+      username: 'postgres',
+      password: '212a418416367de527745b4731f94336',
+      database: 'parking_api_database',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     TypeOrmModule.forFeature([Spot, Booking, Car, User, Rate]),
   ],
   controllers: [BookingController, UserController, SpotController],
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createConnection, ConnectionOptions } from 'typeorm';
 import * as cors from 'cors';
 
-const dbSettings: ConnectionOptions = {
-  type: 'postgres',
-  host: 'lermex.site',
-  port: 18874,
-  username: 'postgres',
-  password: '212a418416367de527745b4731f94336',
-  database: 'parking_api_database',
-  entities: ['src/**/**.entity{.ts,.js}'],
-  synchronize: true,
-};
-
 async function bootstrap() {
-  createConnection(dbSettings)
-  .catch(error => console.log(error));
-
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   await app.listen(process.env.NODE_ENV === 'production' ? 5000 : 3000);
